ee9128c2a23adb25bf686de78a903ef27c08c5c1
PraveenKumarMaheshwaran <mmpraveen96@gmail.com>
2016-09-27 13:55:23 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n,i;
+   scanf("%d",&n);
+   if(n%2==0)
+    printf("%d is a even number",n);
+   else
+    printf("%d is not an even number",n);
+   return 0;
+}
